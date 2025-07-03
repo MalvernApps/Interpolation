23f635151f017b3731599e496b36ac2b92c47ec3
Glenn Murray <glenn@malvern5.plus.com>
2025-07-03 11:31:36 +0100

    improved class

--- inter/Quadratic.cs
+++ inter/Quadratic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot.TickGenerators.TimeUnits;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,29 @@ namespace inter
     public class Quadratic
     {
         // of the standard form ax^2 + bx + c
+        public double a { get; set; }
+        public double b { get; set; }
+        public double c { get; set; }
 
-        double a { get; set; }
-        double b { get; set; }
-        double c { get; set; }
+        public Quadratic(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double CalculateValue(double xValue)
+        {
+            double result;
+            double res = a * xValue * xValue;
+            res += b * xValue;
+            res += c;
+
+            result = res;
+
+            return result;
+
+        }
 
     }
 }
